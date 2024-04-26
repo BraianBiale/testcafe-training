dce529bca4578387a589eecc5aa695ab50571082
Braian Aniel Biale <braiananielbiale@Braians-MacBook-Pro.local>
2024-04-26 16:24:42 -0300

    fix json import

--- tests/roles/roles.js
+++ tests/roles/roles.js
@@ -1,7 +1,7 @@
 import {Role} from 'testcafe';
 import loginPage from "../pages/LoginPage";
 
-const data = require("./data/data.json")
+const data = require("../data/data.json")
 
 export const userOne = Role(`${data.baseUrl}/login`, async t => {
     await loginPage.fillLoginForm(data.validUsers[0].username, data.validUsers[0].password)
